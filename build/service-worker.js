64ce754a991ef25331187468f00b9719b93e4f93
wangyancai156 <wangyancai156@126.com>
2019-05-17 11:26:17 +0800

    添加登录功能

--- build/service-worker.js
+++ build/service-worker.js
@@ -15,7 +15,7 @@ importScripts("./workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "./workbox-v3.6.3"});
 
 importScripts(
-  "./precache-manifest.53d2650d2ab3e3c1637e5738294c6605.js"
+  "./precache-manifest.a2f830cc1e8d9abda3b0160a3bd38606.js"
 );
 
 workbox.clientsClaim();
