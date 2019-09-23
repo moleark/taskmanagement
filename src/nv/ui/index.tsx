773c96a8ba76af2c6a15c2d0268b766d5f5d36d1
wangyancai666 <ycai@jk-sci.com>
2019-09-23 11:29:31 +0800

    设置测试

--- src/nv/ui/index.tsx
+++ src/nv/ui/index.tsx
@@ -1,17 +1,20 @@
 export { jnkTop } from "./jnkTop";
 export { VHome } from './main';
-/*
+
+/*测式*/
 export const GLOABLE = {
     CHINA: 43,
     CHINESE: 197,
     SALESREGION_CN: 4,
     TIPDISPLAYTIME: 2000,
 }
-*/
 
+/*正式*/
+/*
 export const GLOABLE = {
     CHINA: 44,
     CHINESE: 196,
     SALESREGION_CN: 1,
     TIPDISPLAYTIME: 2000,
 }
+*/
