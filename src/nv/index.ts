071961a909c77a8accd46806e0c22568f162d573
ligsh <system6@jkchemical.com>
2019-09-23 11:11:44 +0800

    修改版本为1.0.14

--- src/nv/index.ts
+++ src/nv/index.ts
@@ -7,11 +7,7 @@ export { CApp } from './CApp';
 
 export const appConfig: AppConfig = {
     appName: "百灵威系统工程部/salestask",
-<<<<<<< HEAD
     version: "1.0.14",                   // 版本变化，缓存的uqs才会重载
-=======
-    version: "1.0.13",                   // 版本变化，缓存的uqs才会重载
->>>>>>> 7324ed5cfce37c775f8ad5385148260f66ec1624
     tvs: tvs,
     loginTop: jnkTop,
 };
