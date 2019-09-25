9d24e8f65f6bfae348f325c09b8953b5fb99d99c
wangyancai666 <ycai@jk-sci.com>
2019-09-25 15:59:30 +0800

    Updates

---
+++ precache-manifest.403a438423c0f2ee1fccb0cb6c94e044.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "6a656a1769dc9bdfc0cf",
+    "url": "./static/css/main.f0fb4095.chunk.css"
+  },
+  {
+    "revision": "e16a3ace896c10b6de47",
+    "url": "./static/js/1.e16a3ace.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "c14465881ec9d0496ece",
+    "url": "./static/js/2.c1446588.chunk.js"
+  },
+  {
+    "revision": "a360f86b6d2172f03a22",
+    "url": "./static/js/3.a360f86b.chunk.js"
+  },
+  {
+    "revision": "9196333ee2c8bbe99627",
+    "url": "./static/js/runtime~main.9196333e.js"
+  },
+  {
+    "revision": "6a656a1769dc9bdfc0cf",
+    "url": "./static/js/main.6a656a17.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "c14465881ec9d0496ece",
+    "url": "./static/css/2.89b5548c.chunk.css"
+  },
+  {
+    "revision": "da1538dca3d3f578a7edba5b3c27d5b1",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("./workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "./workbox-v3.6.3"});
 
 importScripts(
-  "./precache-manifest.0c9311f1115b6b5404ea91e8cd2dbec8.js"
+  "./precache-manifest.403a438423c0f2ee1fccb0cb6c94e044.js"
 );
 
 workbox.clientsClaim();
