0251439278159576e96ec6e29a6c9876ea032555
Henry <liaohengyi@outlook.com>
2019-09-26 14:22:40 +0800

    VAchievementDetail

--- src/tonva/app/centerApi.ts
+++
@@ -1,9 +0,0 @@
-import {CenterApiBase} from '../net';
-
-export class CenterApi extends CenterApiBase {
-    async userAppUnits(app:number):Promise<any[]> {
-        return await this.get('tie/user-app-units', {app:app});
-    }
-}
-
-export const centerApi = new CenterApi('tv/', undefined);
