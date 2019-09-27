1a1321c9d71767d15c710850e4e980e9e23ef799
wangyancai666 <ycai@jk-sci.com>
2019-09-27 19:00:49 +0800

    Updates

--- precache-manifest.d82539c699bb7adea817b6e23744af9f.js
+++ precache-manifest.d82539c699bb7adea817b6e23744af9f.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "717b88a0a7880bd8664e",
+    "revision": "efb6fcfdc5988dc5d939",
     "url": "./static/css/main.eface841.chunk.css"
   },
   {
@@ -28,20 +28,20 @@ self.__precacheManifest = [
     "url": "./static/js/runtime~main.5892949d.js"
   },
   {
-    "revision": "717b88a0a7880bd8664e",
-    "url": "./static/js/main.717b88a0.chunk.js"
+    "revision": "efb6fcfdc5988dc5d939",
+    "url": "./static/js/main.efb6fcfd.chunk.js"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "./static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
-  },
-  {
     "revision": "b06871f281fee6b241d60582ae9369b9",
     "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
@@ -52,7 +52,7 @@ self.__precacheManifest = [
     "url": "./static/css/2.006b8015.chunk.css"
   },
   {
-    "revision": "bf1c19066b48266678f174ac08d33acb",
+    "revision": "a29b43b45389ca818e8e7724b4bf2adc",
     "url": "./index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("./workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "./workbox-v3.6.3"});
 
 importScripts(
-  "./precache-manifest.840006837f676bf065afeb0b6f090e4e.js"
+  "./precache-manifest.d82539c699bb7adea817b6e23744af9f.js"
 );
 
 workbox.clientsClaim();
