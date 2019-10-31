431feb4c3e311e07cd133cb67f2725fc3bc7dbbf
wangyancai666 <ycai@jk-sci.com>
2019-10-31 17:56:49 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.36c44c7970bdab29e551ab64b1ae4466.js"
+  "./precache-manifest.678a49cac675480f1fdabd93706d85b1.js"
 );
 
 self.addEventListener('message', (event) => {
