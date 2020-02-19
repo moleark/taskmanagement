4f35150040be3c66bbd5b6830c5e2ec7ab3edc3d
wangyancai156 <wangyancai156@126.com>
2020-02-19 14:18:43 +0800

    处理不显示内容的问题

--- src/post/CPost.tsx
+++ src/post/CPost.tsx
@@ -1,35 +1,9 @@
-import { nav, PageItems, Query } from 'tonva';
-import { CUqBase } from '../CBase';
-import { VPostList } from './VPostList';
-import { observable } from 'mobx';
-import { VPostDetil } from './VPostDetil';
-import { VCustomer } from './VCustomer';
-
-//页面类
-/* eslint-disable */
-/**
- * 用于客户首页
- */
-class PagePost extends PageItems<any> {
-    private searchQuery: Query;
-
-
-    constructor(searchQuery: Query) {
-        super();
-        this.firstSize = this.pageSize = 10;
-        this.searchQuery = searchQuery;
-    }
-
-    protected async load(param: any, pageStart: any, pageSize: number): Promise<any[]> {
-        if (pageStart === undefined) pageStart = 0;
-        let ret = await this.searchQuery.page(param, pageStart, pageSize);
-        return ret;
-    }
-
-    protected setPageStart(item: any): any {
-        this.pageStart = item === undefined ? 0 : item.id;
-    }
-}
+import { nav, PageItems, Query, QueryPager } from "tonva";
+import { CUqBase } from "../CBase";
+import { VPostList } from "./VPostList";
+import { observable } from "mobx";
+import { VPostDetil } from "./VPostDetil";
+import { VCustomer } from "./VCustomer";
 
 //页面类
 /* eslint-disable */
@@ -40,7 +14,11 @@ class PageCustomer extends PageItems<any> {
         this.firstSize = this.pageSize = 10;
         this.searchQuery = searchQuery;
     }
-    protected async load(param: any, pageStart: any, pageSize: number): Promise<any[]> {
+    protected async load(
+        param: any,
+        pageStart: any,
+        pageSize: number
+    ): Promise<any[]> {
         if (pageStart === undefined) pageStart = 0;
         let ret = await this.searchQuery.page(param, pageStart, pageSize);
         return ret;
@@ -51,47 +29,47 @@ class PageCustomer extends PageItems<any> {
 }
 
 export class CPost extends CUqBase {
-    @observable pagePost: PagePost;
+    @observable pagePost: QueryPager<any>;
     @observable pageCustomer: PageCustomer;
     @observable current: any;
     //初始化
     protected async internalStart() {
         nav.clear();
-
     }
 
     //查询客户--通过名称
     searchByKey = async (key: string, author: string) => {
-        this.pagePost = new PagePost(this.uqs.webBuilder.SearchPost);
+        this.pagePost = new QueryPager(this.uqs.webBuilder.SearchPost, 15, 30);
         this.pagePost.first({ key: key, author: author });
-    }
+    };
 
     showPostList = async () => {
-        await this.searchByKey("", "0")
+        await this.searchByKey("", "0");
         this.openVPage(VPostList);
-    }
+    };
 
     showDetail = async (id: number) => {
         this.current = await this.uqs.webBuilder.Post.load(id);
         this.openVPage(VPostDetil);
-    }
+    };
 
     //查询客户--通过名称
     searchCustomerByKey = async (key: string, post: string) => {
-        this.pageCustomer = new PageCustomer(this.uqs.salesTask.SearchMyCustomerByPost);
+        this.pageCustomer = new PageCustomer(
+            this.uqs.salesTask.SearchMyCustomerByPost
+        );
         this.pageCustomer.first({ key: key, post: post });
-    }
+    };
 
     showCustomer = async (key: string, post: any) => {
         await this.searchCustomerByKey(key, post.id);
         this.openVPage(VCustomer, post);
-    }
+    };
 
     addMyCustomerPost = async (post: any, customerlist: any) => {
-
         for (let i: number = 0; i < customerlist.length; i++) {
-            let param = { post: post.id, customer: customerlist[i] }
+            let param = { post: post.id, customer: customerlist[i] };
             await this.uqs.salesTask.AddMyCustomerPost.submit(param);
         }
-    }
+    };
 }
