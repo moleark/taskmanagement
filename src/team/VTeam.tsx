7fc0bdc95c520f37f86e7d88ad0d5116ac07deea
wangyancai666 <ycai@jk-sci.com>
2020-05-12 10:55:42 +0800

    解决不显示客户信息的功能

--- src/team/VTeam.tsx
+++ src/team/VTeam.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { VPage, Page, List, LMR, tv, FA, User, UserView } from 'tonva';
+import { VPage, Page, List, LMR, FA, User, UserView } from 'tonva';
 import { CTeam } from './CTeam';
 import { setting } from 'appConfig';
 
@@ -11,15 +11,8 @@ export class VTeam extends VPage<CTeam> {
         this.openPage(this.page);
     }
 
-    private userSpan(displayName: JSX.Element, assigned: string): JSX.Element {
-        return assigned ?
-            <><b>{assigned} &nbsp; <small className="muted">{displayName}</small></b></>
-            : <b>{displayName}</b>
-    }
-
-
     renderTop = (user: User): JSX.Element => {
-        let { icon, name, nick } = user;
+        let { name, nick } = user;
         return <div>{nick || name}</div>
     }
 
@@ -29,14 +22,13 @@ export class VTeam extends VPage<CTeam> {
         let onshowPeerDetail = async () => await showPeerDetail(teamItem);
         let { children, volume, assigned } = teamItem;
 
-
         return <LMR className="px-3 py-2 " onClick={onshowPeerDetail}
             left={<div> <FA className="text-info px-2" name="user" /></div >}
             right={volume && <div className="w-2c mr-3 text-right" >￥{volume}</div>}>
             <div className="font-weight-bold ">
-                <UserView user={children} render={this.renderTop} />
+                {assigned}
+                {assigned ? <></> : <UserView user={children} render={this.renderTop} />}
             </div>
-
         </LMR >
     }
 
