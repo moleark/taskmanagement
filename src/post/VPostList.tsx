fb2b2af2a8068b17896c72c2c4cf0568962217d1
wangyancai666 <ycai@jk-sci.com>
2020-06-11 11:33:43 +0800

    调整页面

--- src/post/VPostList.tsx
+++ src/post/VPostList.tsx
@@ -74,7 +74,7 @@ export class VPostList extends VPage<CPost> {
                     </div>
                     <div className="d-flex flex-column w-100">
                         <div className="mb-2"><small onClick={() => showPostDetail(item)} >{caption}</small>  </div>
-                        <div className="small d-flex justify-content-between " onClick={() => showPostDetail(item)} >
+                        <div className="small d-flex justify-content-between " onClick={() => showPostDetail(item.post)} >
                             <div className="flex-fill">
                                 <EasyTime date={publishdate} />
                             </div>
