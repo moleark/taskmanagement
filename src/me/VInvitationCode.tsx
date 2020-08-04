214a89ca871aa129651a5aa2d7ac69ca94f62334
wangfeixiang218 <W363@wlrc.com>
2020-08-04 09:37:09 +0800

    优惠活动样式调整

--- src/me/VInvitationCode.tsx
+++ src/me/VInvitationCode.tsx
@@ -69,7 +69,7 @@ export class VInvitationCode extends VPage<CMe> {
         return <Page header='邀请码' headerClassName={setting.pageHeaderCss}>
             <div className="text-center bg-white" style={{ height: "100%" }} >
                 <div id="qrid" className="text-center" style={{ width: 'auto', height: '85%' }}  >
-                    <Image src={setting.sales.logo} className="mt-4" style={{ width: 'auto', height: '23.5rem', margin: '2rem auto, 0 auto' }} />
+                    <Image src={setting.sales.logo} className="mt-2" style={{ width: 'auto', height: '23.5rem', margin: '2rem auto, 0 auto' }} />
                     <div>
                         < QRCode style={{ margin: '2rem 0 0 0' }}
                             value={url}  //value参数为生成二维码的链接
@@ -77,12 +77,12 @@ export class VInvitationCode extends VPage<CMe> {
                             fgColor="#000000"  //二维码的颜色
                         />
                     </div>
-                    <div className="mt-4">
+                    <div className="mt-2">
                         <span className="w-100 text-center h3 m-3 text-info">{inviteCode} </span>
                     </div>
                 </div>
                 <div className="w-100 text-center">
-                    <span className="text-info cursor-info mx-2" onClick={() => this.copyClick(inviteCode)}>复制</span>
+                    <span className="text-info cursor-info mx-2 pb-1" onClick={() => this.copyClick(inviteCode)}>复制</span>
                     {share}
                     <div className="text-center text-white small px-2" style={{ width: '30%', margin: '-80px auto 0 auto', padding: '4px', borderRadius: '3px', backgroundColor: '#505050', display: this.showTips }}>已复制到剪切板</div>
                 </div>
